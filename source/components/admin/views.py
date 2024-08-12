89990a43c143248a4f0870a8dc3a6a9a4baa711d
k.demchenko <k.demchenko@cfunalog.ru>
2024-08-12 13:32:09 +0300

    донастроил фильтр starlette_admin, первый уровень

--- source/components/admin/views.py
+++ source/components/admin/views.py
@@ -2,6 +2,7 @@ from typing import Dict, Any, Optional, List, Sequence, Union, Type
 from starlette.requests import Request
 from starlette_admin import BaseModelView
 from tortoise import Model
+from tortoise.expressions import Q
 
 
 class TortoiseModelView(BaseModelView):
@@ -9,11 +10,59 @@ class TortoiseModelView(BaseModelView):
         super().__init__()
         self.obj = obj
 
+    @staticmethod
+    async def get_expr_from_dict(where: Union[Dict[str, Any]]):
+        # Берем самое верхнее условие
+        cw_o = where.copy().popitem()
+
+        and_or_0 = cw_o[0].upper()
+        list_p_0 = cw_o[1]
+
+        list_expr_0 = []
+        for dict_p in list_p_0:
+            if not dict_p:
+                break
+
+            pf = dict_p.copy().popitem()
+            param = pf[0]
+
+            fv = pf[1].copy().popitem()
+            filter = fv[0]
+            value = fv[1]
+
+            expr = None
+            match filter:
+                case 'eq':
+                    expr = Q(**{param: value})
+                case 'neq':
+                    expr = Q(**{param + "__not": value})
+                case 'lt':
+                    expr = Q(**{param + "__lt": value})
+                case 'le':
+                    expr = Q(**{param + "__lte": value})
+                case 'ge':
+                    expr = Q(**{param + "__gte": value})
+                case 'gt':
+                    expr = Q(**{param + "__gt": value})
+                case 'between':
+                    expr = Q(**{param + "__range": value})
+                case 'not_between':
+                    expr = Q(**{param + "__range__not": value})
+                case 'is_null':
+                    expr = Q(**{param + "__isnull": True})
+                case 'is_not_null':
+                    expr = Q(**{param + "__not_isnull": True})
+
+            list_expr_0.append(expr)
+
+        return Q(*list_expr_0, join_type=and_or_0)
+
     async def find_all(self, request: Request, skip: int = 0, limit: int = 100,
                        where: Union[Dict[str, Any], str, None] = None,
                        order_by: Optional[List[str]] = None) -> Sequence[Any]:
         if (where is not None) and isinstance(where, dict):
-            query_objs = self.obj.filter(**where).all()
+            expr = await self.get_expr_from_dict(where)
+            query_objs = self.obj.filter(expr).all()
         else:
             query_objs = self.obj.all()
 
@@ -36,7 +85,8 @@ class TortoiseModelView(BaseModelView):
     async def count(self, request: Request, where: Union[Dict[str, Any], str, None] = None) -> int:
         if where is not None:
             if isinstance(where, dict):
-                query_objs = self.obj.filter(**where)
+                expr = await self.get_expr_from_dict(where)
+                query_objs = self.obj.filter(expr)
             else:
                 query_objs = self.obj.filter(pk=where)
             return await query_objs.count()
